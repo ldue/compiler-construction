abac9c24531d25af61db445108d75f3aafcd6a41
Lars Dünnwald <larsduennwald@t-online.de>
2017-04-10 09:45:18 +0200

    update gitignore

---
+++ cmake-build-debug/CMakeFiles/compiler_construction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler_construction.dir/main.cpp.o"
+  "compiler_construction.pdb"
+  "compiler_construction.exe"
+  "libcompiler_construction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler_construction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
