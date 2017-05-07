e7c7183af63ed374d4ad45f969fec9bd181463cc
Kai <kai@naschinski.de>
2017-05-07 13:04:45 +0200

    -include tree

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 // Created by Lars on 10.04.2017.
 //
 #include <iostream>
+#include <string>
+#include "st_tree.h"
+#include <iostream>
 
 int main () {
-    std::cout << "hello";
+    st_tree::tree<std::string> t;
+    typedef st_tree::tree<std::string>::iterator iterator;
+    t.insert("Hello");
+    t.root().insert(" ");
+    t.root().insert("World");
+
+    for (iterator j(t.begin());  j != t.end();  ++j)
+        std::cout << j->data();
+
+
     return 0;
 }
