76644b04ec0a1c6bdd3e44f42c3cbac40e06ef10
Kai <kai@naschinski.de>
2017-05-08 08:37:31 +0200

    -

--- main.cpp
+++ main.cpp
@@ -7,13 +7,16 @@
 
 int main(int argc, char *argv[]) {
     //"/cygdrive/c/Users/Naschinsui/CLionProjects/compiler-construction/test/samplefiles/"
-    for (int i = 1; i < argc; i++) {
-        Lexer *lex = new Lexer(argv[i]);
+    if (argc) {
+        Lexer *lex = new Lexer(argv[1]);
         std::vector<std::string> *symTab = lex->getSymbolTable();
         Parser *parser = new Parser(lex);
         parser->start();
         parser->printAST(symTab);
         std::cout << "=====================================================" << std::endl;
+    } else {
+        std::string line= "";
+        std::cin>> line;
     }
     /*do {
         curTok = lex->getNextTok();
