bd7f97588550f76859d1ae7303fc451a01297818
ldue <larsduennwald@gmail.com>
2017-05-08 08:50:55 +0200

    add test.sh for testing

---
+++ test/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILES=./test/samplefiles/*
+
+for f in $FILES
+do
+  echo "Processing $f file..."
+  # take action on each file. $f store current file name
+  ./cmake-build-debug/compiler_construction.exe $f
+ done
